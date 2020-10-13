b5af1d2b6dc412afabf7c9d7d4c5b429599851e9
Jenny Tran <jdtran.art@gmail.com>
2020-10-13 12:13:17 -0700

    javascript-data-model done...I think

---
+++ javascript-data-model/main.js
@@ -0,0 +1,124 @@
+/* exported orderHistory */
+var orderHistory = [
+  {
+    summary: {
+      datePlaced: 'Order Placed August 4, 2020',
+      grandTotal: '$34.00',
+      shipCustomer: 'Ship to JS Masher',
+      orderNumber: 'Order # 114-3941689-8772232',
+      details: 'Order details',
+      invoice: 'Invoice'
+    },
+    dateDelivered: 'Delivered Aug, 8, 2020',
+    deliveryMethod: null,
+    itemCartList: [
+      {
+        productBook: {
+          image: 'Image: JavaScript for impatient programmers',
+          name: 'JavaScript for impatient programmers',
+          author: 'Rauschmayer, Dr. Axel',
+          dateReturnBy: 'Return window closed on Aug 19, 2020',
+          price: '$31.55',
+          buyAgain: 'Buy it again',
+          viewAgain: 'View your item '
+        }
+      }
+    ],
+    itemSupport: null,
+    itemReview: 'Write a product review'
+  },
+
+  {
+    summary: {
+      datePlaced: 'Order Placed July 19, 2020',
+      grandTotal: '$44.53',
+      shipCustomer: 'Ship to JS Masher',
+      orderNumber: 'Order # 113-9984268-1280257',
+      details: 'Order details',
+      invoice: 'Invoice'
+    },
+    dateDelivered: 'Delivered Jul 20, 2020',
+    deliveryMethod: 'Your package was delivered. It was handed directly to a resident',
+    itemCartList: [
+      {
+        productBook: {
+          image: 'Image: The Timeless Way of Building',
+          name: 'The Timeless Way of Building',
+          author: 'Alexander, Christopher',
+          dateReturnBy: 'Return window closed on Aug 19, 2020',
+          price: '$41.33',
+          buyAgain: 'Buy it again',
+          viewAgain: 'View your item '
+        }
+      }
+    ],
+    itemSupport: null,
+    itemReview: 'Write a product review'
+  },
+
+  {
+    summary: {
+      datePlaced: 'Order Placed July 4, 2020',
+      grandTotal: '$17.22',
+      shipCustomer: 'Ship to JS Masher',
+      orderNumber: 'Order # 114-2875557-9059409',
+      details: 'Order details',
+      invoice: 'Invoice'
+    },
+    dateDelivered: 'Delivered Jul 7, 2020',
+    deliveryMethod: 'Your package was delivered. It was handed directly to a resident',
+    productList: [
+      {
+        productTech: {
+          image: 'Image: Gamecube Controller Adapter',
+          name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+          dateReturnBy: 'Return window closed on Aug 19, 2020',
+          price: '$15.98',
+          buyAgain: 'Buy it again',
+          viewAgain: 'View your item '
+        }
+      }
+    ],
+    itemSupport: null,
+    itemReview: 'Write a product review'
+  },
+
+  {
+    summary: {
+      datePlaced: 'Order Placed July 3, 2020',
+      grandTotal: '$138.93',
+      shipCustomer: 'Ship to JS Masher',
+      orderNumber: 'Order # 113-2883177-2648248',
+      details: 'Order details',
+      invoice: 'Invoice'
+    },
+    dateDelivered: 'Delivered Jul 5, 2020',
+    deliveryMethod: null,
+    itemCartList: [
+      {
+        productTech: {
+          image: 'Image: Gamecube Controller',
+          name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+          dateReturnBy: 'Return window closed on Aug 4, 2020',
+          price: '$94.95',
+          buyAgain: 'Buy it again',
+          viewAgain: 'View your item ',
+          productReview: 'Write a product review'
+        }
+      },
+      {
+        productBook: {
+          image: 'Image: The Art of Sql',
+          name: 'The Art of Sql',
+          author: 'Faroult, Stephane',
+          dateReturnBy: 'Return window closed on Aug 4, 2020',
+          price: '$33.99',
+          buyAgain: 'Buy it again',
+          viewAgain: 'View your item '
+        }
+      }
+    ],
+    itemReview: 'Write a product review',
+    itemSupport: 'Get product support'
+  }
+];
