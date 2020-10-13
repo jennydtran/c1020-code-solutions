ffb74ea06858ca91d3033b3dc27eb54fc396fa9c
Jenny Tran <jdtran.art@gmail.com>
2020-10-13 15:14:14 -0700

    took out unneccessary strings/ shortened the strings

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,14 +2,14 @@
 var orderHistory = [
   {
     summary: {
-      datePlaced: 'Order Placed August 4, 2020',
-      grandTotal: '$34.00',
-      shipCustomer: 'Ship to JS Masher',
-      orderNumber: 'Order # 114-3941689-8772232',
+      datePlaced: 'August 4, 2020',
+      grandTotal: 34.00,
+      shipCustomer: 'JS Masher',
+      orderNumber: '114-3941689-8772232',
       details: 'Order details',
       invoice: 'Invoice'
     },
-    dateDelivered: 'Delivered Aug, 8, 2020',
+    dateDelivered: 'Aug, 8, 2020',
     deliveryMethod: null,
     itemCartList: [
       {
@@ -18,7 +18,7 @@ var orderHistory = [
           name: 'JavaScript for impatient programmers',
           author: 'Rauschmayer, Dr. Axel',
           dateReturnBy: 'Return window closed on Aug 19, 2020',
-          price: '$31.55',
+          price: 31.55,
           buyAgain: 'Buy it again',
           viewAgain: 'View your item '
         }
@@ -30,14 +30,14 @@ var orderHistory = [
 
   {
     summary: {
-      datePlaced: 'Order Placed July 19, 2020',
-      grandTotal: '$44.53',
-      shipCustomer: 'Ship to JS Masher',
-      orderNumber: 'Order # 113-9984268-1280257',
+      datePlaced: 'July 19, 2020',
+      grandTotal: 44.53,
+      shipCustomer: 'JS Masher',
+      orderNumber: '113-9984268-1280257',
       details: 'Order details',
       invoice: 'Invoice'
     },
-    dateDelivered: 'Delivered Jul 20, 2020',
+    dateDelivered: 'Jul 20, 2020',
     deliveryMethod: 'Your package was delivered. It was handed directly to a resident',
     itemCartList: [
       {
@@ -45,8 +45,8 @@ var orderHistory = [
           image: 'Image: The Timeless Way of Building',
           name: 'The Timeless Way of Building',
           author: 'Alexander, Christopher',
-          dateReturnBy: 'Return window closed on Aug 19, 2020',
-          price: '$41.33',
+          dateReturnBy: 'Aug 19, 2020',
+          price: 41.33,
           buyAgain: 'Buy it again',
           viewAgain: 'View your item '
         }
@@ -58,22 +58,22 @@ var orderHistory = [
 
   {
     summary: {
-      datePlaced: 'Order Placed July 4, 2020',
-      grandTotal: '$17.22',
-      shipCustomer: 'Ship to JS Masher',
-      orderNumber: 'Order # 114-2875557-9059409',
+      datePlaced: 'July 4, 2020',
+      grandTotal: 17.22,
+      shipCustomer: 'JS Masher',
+      orderNumber: '114-2875557-9059409',
       details: 'Order details',
       invoice: 'Invoice'
     },
-    dateDelivered: 'Delivered Jul 7, 2020',
+    dateDelivered: 'Jul 7, 2020',
     deliveryMethod: 'Your package was delivered. It was handed directly to a resident',
     productList: [
       {
         productTech: {
           image: 'Image: Gamecube Controller Adapter',
           name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-          dateReturnBy: 'Return window closed on Aug 19, 2020',
-          price: '$15.98',
+          dateReturnBy: 'Aug 19, 2020',
+          price: 15.98,
           buyAgain: 'Buy it again',
           viewAgain: 'View your item '
         }
@@ -85,22 +85,22 @@ var orderHistory = [
 
   {
     summary: {
-      datePlaced: 'Order Placed July 3, 2020',
-      grandTotal: '$138.93',
-      shipCustomer: 'Ship to JS Masher',
-      orderNumber: 'Order # 113-2883177-2648248',
+      datePlaced: 'July 3, 2020',
+      grandTotal: 138.93,
+      shipCustomer: 'JS Masher',
+      orderNumber: '113-2883177-2648248',
       details: 'Order details',
       invoice: 'Invoice'
     },
-    dateDelivered: 'Delivered Jul 5, 2020',
+    dateDelivered: 'Jul 5, 2020',
     deliveryMethod: null,
     itemCartList: [
       {
         productTech: {
           image: 'Image: Gamecube Controller',
           name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-          dateReturnBy: 'Return window closed on Aug 4, 2020',
-          price: '$94.95',
+          dateReturnBy: 'Aug 4, 2020',
+          price: 94.95,
           buyAgain: 'Buy it again',
           viewAgain: 'View your item '
         }
@@ -110,8 +110,8 @@ var orderHistory = [
           image: 'Image: The Art of Sql',
           name: 'The Art of Sql',
           author: 'Faroult, Stephane',
-          dateReturnBy: 'Return window closed on Aug 4, 2020',
-          price: '$33.99',
+          dateReturnBy: 'Aug 4, 2020',
+          price: 33.99,
           buyAgain: 'Buy it again',
           viewAgain: 'View your item '
         }
