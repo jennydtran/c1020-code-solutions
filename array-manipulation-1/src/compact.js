98d90003bbe8740e9a4650765646295299453337
Jenny Tran <jdtran.art@gmail.com>
2020-10-17 14:17:02 -0700

    array-manipulation 1 completed. LVLed up.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-vars */
 function compact(array) {
   var result = [];
-  var truthy = 1 < 0;
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== truthy) {
-      return result.push(array[i]);
+    if (array[i]) {
+      result.push(array[i]);
     }
   }
   return result;
