53dd022a9180879df628c8d5b1e23ebf86ad0a34
Jenny Tran <jdtran.art@gmail.com>
2020-10-19 14:31:00 -0700

    fixed reverse function

--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -2,9 +2,8 @@
 function reverse(array) {
   var result = [];
   var last = array.length - 1;
-  for (var i = last; i >= 0;) {
+  for (var i = last; i >= 0; i--) {
     result.push(array[i]);
-    i--;
   }
   return result;
 }
