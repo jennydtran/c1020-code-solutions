de7ff964d2d25c0343b6acfab314c73789cd6716
Jenny Tran <jdtran.art@gmail.com>
2020-10-22 15:47:46 -0700

    dom event delegation revised. no more loops

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,21 +1,15 @@
 var taskList = document.querySelector('.task-list');
-var allButtons = document.querySelectorAll('button');
 
 taskList.addEventListener('click', function (event) {
-  var buttonObject;
-  for (var i = 0; i < allButtons.length; i++) {
-
-    buttonObject = allButtons[i];
-
-    var li = buttonObject.closest('.task-list-item');
-
-    if (event.target === buttonObject) {
-      console.log('closest .task-list-item', li);
-      li.remove();
-    }
-  }
 
   console.log('event.target', event.target);
   console.log('event.target.tagName', event.target.tagName);
+
+  var li = event.target.closest('.task-list-item');
+
+  if (event.target.tagName === 'BUTTON') {
+    console.log('closest .task-list-item', li);
+    li.remove();
+  }
 
 });
