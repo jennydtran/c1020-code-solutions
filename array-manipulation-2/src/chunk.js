179a610098710445a4a58db755ca40703e58b11e
Jenny Tran <jdtran.art@gmail.com>
2020-11-08 14:19:24 -0800

    array chunk completed

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,24 +2,13 @@
 function chunk(array, size) {
   var result = [];
   var subArray = [];
-  /*  if (size >= array.length) {
-    return [];
-  } else { */
-  var i = 0;
-  for (i; i < array.length + size; i++) {
-    for (i = 0; i < size; i++) {
+  for (var i = 0; i < array.length; i++) {
+    subArray = result[result.length - 1];
+    if (!subArray || subArray.length === size) {
+      result.push([array[i]]);
+    } else {
       subArray.push(array[i]);
     }
-    result.push(subArray);
   }
-  //  }
-  return result.push(subArray[i]);
+  return result;
 }
-/* chunk(['foo', 'bar', 'baz', 'qux'], 2);
-// -> [["foo", "bar"], ["baz", "qux"]]
-chunk([1, 2, 3, 4, 5], 1);
-// -> [[1], [2], [3], [4], [5]]
-chunk([false, true, false, true], 3);
-// -> [[false, true, false], [true]]
-chunk([], 7);
-// -> [] */
