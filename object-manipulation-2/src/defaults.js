3130567fa0cb7786216c594c715a6a1f7b3714b7
Jenny Tran <jdtran.art@gmail.com>
2020-11-12 18:49:58 -0800

    object-manipulation-2 completed

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,24 +1,8 @@
 /* eslint-disable no-unused-vars */
 function defaults(target, source) {
   for (var key in source) {
-    // copy all source to target
-    // if undefined do copy
     if (target[key] === undefined) {
       target[key] = source[key];
     }
   }
-
-  // for (var property in target) {
-  //   var include = false;
-  //   for (var prop in source) {
-  //     if (property === prop || source[prop] === undefined) {
-  //       include = true;
-  //       break;
-  //     }
-  //   }
-  //   if (!include || target[property] === undefined) {
-  //     target[property] = source[property];
-  //   }
-  // }
-  // return target;
 }
