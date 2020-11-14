dfa7d999f59d71a4d7e2639b9637927266597f57
Jenny Tran <jdtran.art@gmail.com>
2020-11-13 22:50:51 -0800

    finished 5/8 algorithms

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,4 +1,30 @@
 /* eslint-disable no-unused-vars */
 function numVowels(string) {
-
+  var newString = string.toLowerCase();
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var vArray = [];
+  for (var vowel of vowels) {
+    for (var letters of newString) {
+      if (vowel === letters) {
+        vArray.push(vowel);
+      }
+    }
+  }
+  return vArray.length;
 }
+
+/* Second solution
+function numVowels(string) {
+  var newString = string.toLowerCase();
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var vArray = [];
+  for (var letters of newString) {
+    for (var i = 0; i < vowels.length; i++) {
+    if (vowels[i].match(letters)) {
+        vArray.push(vowels[i]);
+      }
+    }
+  }
+  return vArray.length;
+}
+*/
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
 function ransomCase(string) {
+  var newString = string.toLowerCase();
 
 }
