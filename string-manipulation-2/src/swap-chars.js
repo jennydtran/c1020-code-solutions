4cf01fa526d438b792e2cbd1bd1fdeb1c9480cda
Jenny Tran <jdtran.art@gmail.com>
2020-11-14 21:17:12 -0800

    solved swapChars

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,2 +1,7 @@
 /* eslint-disable no-unused-vars */
-function swapChars(firstIndex, secondIndex, string) {}
+function swapChars(firstIndex, secondIndex, string) {
+  var newString = string.split('');
+  newString[firstIndex] = string[secondIndex];
+  newString[secondIndex] = string[firstIndex];
+  return newString.join('');
+}
