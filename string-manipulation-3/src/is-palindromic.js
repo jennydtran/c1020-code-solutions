af1465e535f14774d45a66f09ae663d983a2bbb4
Jenny Tran <jdtran.art@gmail.com>
2020-11-15 12:16:22 -0800

    fixed isPalindromic

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,18 +1,13 @@
 /* eslint-disable no-unused-vars */
 function isPalindromic(string) {
   var i;
-  for (i = 0; i < string.length; i++) {
-    var newString = string.replace(' ', '');
-  }
-
+  var newString = string.replaceAll(' ', '');
   var backwards = newString.length - 1;
   for (i = 0; i < newString.length; i++) {
     if (newString[i] !== newString[backwards]) {
       return false;
-    } else if (newString[i] === newString[backwards]) {
-      var match = true;
     }
     backwards--;
   }
-  return match;
+  return true;
 }
