00cfed4becf5ff43201088e666c576a0e9f70934
Jenny Tran <jdtran.art@gmail.com>
2020-11-14 21:38:18 -0800

    completed all tests

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-unused-vars */
 function capitalizeWords(string) {
-
+  var newString = string.toLowerCase();
+  newString = newString.split(' ');
+  for (var i = 0; i < newString.length; i++) {
+    newString[i] = newString[i].replace(newString[i][0], newString[i][0].toUpperCase());
+  }
+  return newString.join(' ');
 }
