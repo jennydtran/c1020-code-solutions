bbe843e799a1c112b9f821c50d49b2782b258fbb
Jenny Tran <jdtran.art@gmail.com>
2020-11-20 22:32:32 -0800

    solved union. completed array manipulation 3

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+function union(first, second) {
+  var newArray = first;
+  for (var i = 0; i < second.length; i++) {
+    if (!newArray.includes(second[i])) {
+      newArray.push(second[i]);
+    }
+  }
+  return newArray;
+}
