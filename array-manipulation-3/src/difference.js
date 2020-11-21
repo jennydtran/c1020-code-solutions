6a76e3a031336173c5d17421df77264175f32cda
Jenny Tran <jdtran.art@gmail.com>
2020-11-20 17:33:37 -0800

    solved difference

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+function difference(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (!first.includes(second[j])) {
+      newArray.push(second[j]);
+    }
+  }
+  return newArray;
+}
