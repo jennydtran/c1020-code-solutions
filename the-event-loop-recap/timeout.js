64615c04611716bd67972329534cefa150f8f22e
Jenny Tran <jdtran.art@gmail.com>
2020-11-25 22:56:50 +0000

    the-event-loop-recap completed

---
+++ the-event-loop-recap/timeout.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+console.log('Hello, just a moment...');
+
+function delayedMessage() {
+  console.log('Thanks for waiting!');
+}
+
+setTimeout(delayedMessage, 2000);
