50277603d1af44e3f3bc300b04c66a1a7bd8b0b3
Jenny Tran <jdtran.art@gmail.com>
2020-11-28 00:10:01 +0000

    finished level 3 node-fs-readfile

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-console */
 const fs = require('fs');
+const i = 2;
 
-for (var i = 2; i < process.argv.length; i++) {
-  fs.readFile(process.argv[i], 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
+const readFiles = function (i) {
+  if (i !== process.argv.length) {
+    fs.readFile(process.argv[i], 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        readFiles(i + 1);
+      }
+    });
+  }
+};
+readFiles(i);
