fae19b737792daa83509ad35afc7e93a9607ae7b
Jenny Tran <jdtran.art@gmail.com>
2020-12-07 00:52:10 +0000

    took out comments

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,9 +1,3 @@
-/* List the 5 most profitable DVD titles in inventory. Profit is the amount of money made past the replacement cost. Don't forget, there are multiple copies of each film in inventory. You will likely benefit from first calculating the cost of each film and the revenue of each film in separate common table expressions.*/
--- go to films to get replacementCost, rental cost, filmId, and filmTitle
--- go to payments > sum(amounts) and group by rental id
--- go to rentals >  get inventoryId and rentalId to connect to payments in rentalId
--- PROFIT = revenue - cost
--- revenue = amount per film
 with cte_filmCost as (
   select "films"."title" as "title",
          "films"."replacementCost",
