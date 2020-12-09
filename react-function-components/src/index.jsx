488fdeae4488b0abe73863e12503aba0021d6c94
Jenny Tran <jdtran.art@gmail.com>
2020-12-09 18:58:47 +0000

    React Function Components completed

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function CustomButton() {
+  return <button>Click Me!</button>;
+}
+
+const root = document.querySelector('#root');
+
+ReactDom.render(CustomButton(), root);
