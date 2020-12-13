a6cfe2961e95b58dcc3632edb00fca6694d53def
Jenny Tran <jdtran.art@gmail.com>
2020-12-13 20:13:13 +0000

    Changes for pull request.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -2,64 +2,76 @@ import React from 'react';
 
 export const navMenu = ['About', 'Get Started', 'Sign In'];
 
+export function ListNavItem(props) {
+  return (
+    <a href='#' onClick={props.onClick}>
+      <li>{props.value}</li>
+    </a>
+  );
+}
+
+export function ListNav(props) {
+  const ListNavItems = navMenu.map(navitem =>
+    <ListNavItem key={navMenu.indexOf(navitem).toString()} value={navitem} onClick={props.onClick} />
+  );
+  return (
+    <ul>{ListNavItems}</ul>
+  );
+}
+
+export function Icon(props) {
+  return (
+    <div className="icon">
+      <i className="fas fa-bars fa-2x" onClick={props.onClick}></i>
+    </div>
+  );
+}
+
 export class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      menuView: false,
-      navToggle: 'off',
-      overlayToggle: ' none'
+      menuView: false
     };
+    this.setState = this.setState.bind(this);
     this.handleClickOn = this.handleClickOn.bind(this);
     this.handleClickOff = this.handleClickOff.bind(this);
   }
 
   handleClickOn() {
-    this.setState(state => ({
-      menuView: true,
-      navToggle: 'on',
-      overlayToggle: ' overlay'
-    }));
+    this.setState({
+      menuView: true
+    });
   }
 
   handleClickOff() {
-    this.setState(state => ({
-      menuView: false,
-      navToggle: 'off',
-      overlayToggle: ' none'
-    }));
+    this.setState({
+      menuView: false
+    });
   }
 
   render() {
-    const ListNavItem = props => {
-      return (
-        <a href='#' onClick={this.handleClickOff}>
-          <li>{props.value}</li>
-        </a>
-      );
-    };
+    let navToggle;
+    let overlayToggle;
 
-    const ListNav = props => {
-      const ListNavItems = navMenu.map(navitem =>
-        <ListNavItem key={navMenu.indexOf(navitem).toString()} value={navitem} />
-      );
-      return (
-        <ul>{ListNavItems}</ul>
-      );
-    };
+    if (this.state.menuView) {
+      navToggle = 'on';
+      overlayToggle = ' overlay';
+    } else if (!this.state.menuView) {
+      navToggle = 'off';
+      overlayToggle = ' none';
+    }
 
     return (
       <div>
         <div>
-          <div className={`container${this.state.overlayToggle}`} onClick={this.handleClickOff}>
+          <div className={`container${overlayToggle}`} onClick={this.handleClickOff}>
           </div>
-          <div className="icon">
-            <i className="fas fa-bars fa-2x" onClick={this.handleClickOn}></i>
-          </div>
+          <Icon onClick={this.handleClickOn}/>
         </div>
-        <div className={`menu-list ${this.state.navToggle}`}>
+        <div className={`menu-list ${navToggle}`}>
           <h2>Menu</h2>
-          <ListNav />
+          <ListNav onClick={this.handleClickOff}/>
         </div>
       </div>
     );
--- react-app-drawer-component/src/index.jsx
+++ react-app-drawer-component/src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as AppDrawer from './app-drawer';
 
+const navMenu = ['About', 'Get Started', 'Sign In'];
+
 ReactDOM.render(
-  <AppDrawer.AppDrawer />,
+  <AppDrawer.AppDrawer navMenu={navMenu}/>,
   document.querySelector('#root')
 );
