e429887d43f892b2014fa31beda8dbca630dcdfa
Jenny Tran <jdtran.art@gmail.com>
2020-12-14 09:37:40 +0000

    React Validated Input Component completed

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -11,14 +11,40 @@ export default class ValidatedInput extends React.Component {
   }
 
   validatePassword(event) {
+    const regexLowercase = new RegExp('^(?=.*[a-z])');
+    const regexUppercase = new RegExp('^(?=.*[A-Z])');
+    const regexNumber = new RegExp('^(?=.*[0-9])');
+    const regexSpecial = new RegExp('^(?=.*[!@#$%^&*()])');
+    const regexEight = new RegExp('^(?=.{8,})');
+
     if (event.target.value === '') {
       this.setState(state => ({
         errorMessage: 'A password is required.',
         icon: 'fas fa-times'
       }));
-    } else if (event.target.value.length < 8) {
+    } else if (regexEight.test(event.target.value) === false) {
       this.setState(state => ({
-        errorMessage: 'Your password is too short.',
+        errorMessage: 'Your password must be at least 8 characters long.',
+        icon: 'fas fa-times'
+      }));
+    } else if (regexSpecial.test(event.target.value) === false) {
+      this.setState(state => ({
+        errorMessage: 'Your password must contain at least one special character.',
+        icon: 'fas fa-times'
+      }));
+    } else if (regexNumber.test(event.target.value) === false) {
+      this.setState(state => ({
+        errorMessage: 'Your password must contain at least one number.',
+        icon: 'fas fa-times'
+      }));
+    } else if (regexUppercase.test(event.target.value) === false) {
+      this.setState(state => ({
+        errorMessage: 'Your password must contain at least one uppercase letter.',
+        icon: 'fas fa-times'
+      }));
+    } else if (regexLowercase.test(event.target.value) === false) {
+      this.setState(state => ({
+        errorMessage: 'Your password must contain at least one lowercase letter.',
         icon: 'fas fa-times'
       }));
     } else {
@@ -30,10 +56,10 @@ export default class ValidatedInput extends React.Component {
   }
 
   handleChange(event) {
-    this.validatePassword(event);
-    this.setState({
+    this.setState(state => ({
       passwordValue: event.target.value
-    });
+    }));
+    this.validatePassword(event);
   }
 
   render() {
